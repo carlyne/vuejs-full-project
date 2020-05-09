f4ccd3552d8fae870e57cbcfbeebbe0d9253ced4
Carly <carlyne.lapierre@gmail.com>
2020-05-09 15:14:58 +0200

    add img and some flex. next step : layout and styling

--- src/views/about/About.vue
+++ src/views/about/About.vue
@@ -3,28 +3,31 @@
     <h1>This is an about page</h1>
 
     <main class="view-grid v-row v-center">
-        <img alt="photo">
-        <section class="view-grid v-column">
-          <header class="view-grid v-row">
-            <p>Dr Horrible</p>
-            <p>Pr.</p>
-          </header>
+      <figure>
+        <img src="https://cataas.com/cat?w=500&amp;h=500" alt="photo">
+      </figure>
 
-         <section class="view-grid v-row">
-           <p>Age</p>
-           <p>27 years old</p>
-         </section>
+      <section class="view-grid v-column">
+        <header class="view-grid v-row">
+          <p>Dr Horrible</p>
+          <p>Pr.</p>
+        </header>
 
-         <section class="view-grid v-row">
-           <p>Things I love</p>
-           <p>spaceship</p>
-         </section>
-
+        <section class="view-grid v-row">
+          <p>Age</p>  
+          <p>27 years old</p>
         </section>
-        <aside>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aliquam, ullam reiciendis aut doloribus maiores nesciunt dignissimos 
-          repellat sapiente quae quod a. Sint, eaque velit. Fugit tempore quidem numquam ea facilis.
-        </aside>
+
+        <section class="view-grid v-row">
+          <p>Things I love</p>
+          <p>spaceship</p>
+        </section>
+      </section>
+
+      <aside>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aliquam, ullam reiciendis aut doloribus maiores nesciunt dignissimos 
+        repellat sapiente quae quod a. Sint, eaque velit. Fugit tempore quidem numquam ea facilis.
+      </aside>
     </main>
   </div>
 </template>
